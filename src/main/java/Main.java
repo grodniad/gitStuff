fd336de5e88a5ce6934e80e817c722209d965063
grodniad <33559062+grodniad@users.noreply.github.com>
2022-10-17 11:14:16 +0300

    TASK-0001 Added more souts

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,9 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("abcd");
+        System.out.println("abcd");
+        System.out.println("abcd");
+        System.out.println("abcd");
+        System.out.println("abcd");
     }
 }
