efc642e7a3f8dfc2572d8d21232be57e0280a7e5
cbalta <claudiubalta@gmail.com>
2022-10-17 11:18:56 +0300

    TASK-0000 Updated after review

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("abcd");
         System.out.println("1234");
-        System.out.println("5432");
+        System.out.println("9876");
         System.out.println("abcd");
         System.out.println("6543");
     }
